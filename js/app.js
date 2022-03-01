3c708130d89a43d89cd43d89825d1f5202713478
khanmohammadimran <khanmohammadimran.i31@gmail.com>
2022-03-01 21:47:14 +0600

    single phone details section is completed

--- js/app.js
+++ js/app.js
@@ -8,8 +8,10 @@ const searchPhone = () => {
   if (searchText == "") {
     error.innerText = 'Please Give a Number'
   }
-  // console.log(searchText);
+  // Clear Data 
   searchfield.value = "";
+
+  // Load Data 
   const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
   fetch(url)
     .then(res => res.json())
@@ -54,15 +56,41 @@ const displaySinglePhoneDetail = phone => {
   const phoneDetails = document.getElementById('single-phone-detail');
   const div = document.createElement('div')
   div.classList.add('card')
-  div.classList.add("col-lg-4")
-  div.classList.add("col-sm-12")
   div.innerHTML = `
-  <img src="..." class="img-fluid rounded-start" alt="...">
-  <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-      </div>
+  <div class="row g-0">
+    <div class="col-lg-8 col-sm-12 d-flex align-items-center">
+        <img src="${phone.image}" class="rounded mx-auto d-block" alt="...">
+    </div>
+    <div class="col-lg-4 col-sm-12">
+        <div class="mt-3 p-3">
+            <h5 class="card-title">Brand: ${phone.brand}</h5>
+            <hr>
+            <h5 class="card-text">Name: ${phone.name}</h5>
+            <hr>
+            <h5 class="card-text">Display: ${phone.mainFeatures.storage}</h5>
+            <hr>
+            <h5 class="card-text">Display: ${phone.mainFeatures.displaySize}</h5>
+            <hr>
+            <h5 class="card-text">Chipset: ${phone.mainFeatures.chipSet}</h5>
+            <hr>
+            <h5 class="card-text">Memeory: ${phone.mainFeatures.memory}</h5>
+            <hr>
+            <h5 class="card-text">Sensors: ${phone.mainFeatures.sensors}</h5>
+            <hr>
+            <h3>Other Feature</h3>
+            <h5 class="card-text">WLAN: ${phone.others.WLAN}</h5>
+            <hr>
+            <h5 class="card-text">Bluetooth: ${phone.others.Bluetooth}</h5>
+            <hr>
+            <h5 class="card-text">GPS: ${phone.others.GPS}</h5>
+            <hr>
+            <h5 class="card-text">NFC: ${phone.others.NFC}</h5>
+            <hr>
+            <h5 class="card-text">USB: ${phone.others.USB}</h5>
+            <hr>
+        </div>
+    </div>
+</div>
   `
   phoneDetails.appendChild(div)
 }
